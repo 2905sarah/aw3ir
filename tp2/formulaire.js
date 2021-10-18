1f795be840ca69817eb745a315ee84b97d5596a4
2905sarah <hamdounesarah212@gmail.com>
2021-10-18 22:20:08 +0200

    Update

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -5,23 +5,20 @@ function validation(){
   
     document.getElementById("error").innerHTML = "";
   
-    for(let item of document.querySelectorAll('.inputAW')){
-      if(item.value.length < 5){
-        // recuperation du label
-        var label =   document.querySelector("label[for="+item.id+"]").textContent;
-        // ajout du message d'erreur
-        document.getElementById("error").innerHTML += `La saisie de ${label} est obligatoire <br/>`;
-        
-        // ajout de la classe CSS "display"
+    for(let item of document.querySelectorAll('.form-control')){
+
+      if(item.value.length < 5) {       
+        var label =   document.querySelector("label[for="+item.id+"]").textContent;       
+        document.getElementById("error").innerHTML += `${label} doit contenir au moins 5 caratères <br/>`;       
         document.getElementById("error").classList.add("display");
         document.getElementById("Resultat").classList.remove("display")
     
       }
         
     }
-      if(document.getElementById("error").innerHTML == ""){
+      if(document.getElementById("error").innerHTML == "") {
         var nom = document.querySelector("#Nom").value;
-        document.getElementById("Resultat").innerHTML = `Bienvenue à ${nom}`;
+        document.getElementById("Resultat").innerHTML = `Bienvenue ${nom}`;
         document.getElementById("error").classList.remove("display")
         document.getElementById("Resultat").classList.add("display")
       }
