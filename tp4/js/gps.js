ba407481288a4a7c838c732d8478eff56afd139f
2905sarah <hamdounesarah212@gmail.com>
2022-01-10 12:24:37 +0100

    upload

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -13,6 +13,7 @@ function showPosition(position) {
     var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg`;
     
     $("#map").html(`<img src='${img_url}'>`);
+    $("#Adresse").val(latlon);
 }
 
 // Au cas ou l'utilisateur refuse
